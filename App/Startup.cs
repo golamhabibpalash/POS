2520d5be8de31dec8a6c99fcf1a33df35b4bd421
golam habib palash <golamhabibpalash@gmail.com>
2021-10-14 14:01:47 +0600

    Utilities Folderize

--- App/Startup.cs
+++ App/Startup.cs
@@ -1,6 +1,8 @@
+using DB;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -24,12 +26,21 @@ namespace APP
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            services.AddControllersWithViews()
+                .AddRazorRuntimeCompilation();
+
+            services.AddRazorPages();
+
             services.AddDistributedMemoryCache();
 
             services.AddSession(options =>
             {
                 options.IdleTimeout = TimeSpan.FromMinutes(30);
             });
+
+            services.AddDbContext<POSDbContext>(options =>
+                  options.UseSqlServer(
+                      Configuration.GetConnectionString("POSDBConnection")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -52,6 +63,8 @@ namespace APP
 
             app.UseAuthorization();
 
+
+            app.UseSession();
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllerRoute(
