59cb67c4a15b95f47f11c54e1d67e3ced417070d
golam habib palash <golamhabibpalash@hotmail.com>
2021-10-15 23:59:18 +0600

    Administrations Controller added

---
+++ APP/Controllers/AdministratorsController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace APP.Controllers
+{
+    public class AdministratorsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
