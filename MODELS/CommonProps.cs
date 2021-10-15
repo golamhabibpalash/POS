102db8b425eb5b288fecdf90f217d1327fb1b55f
golam habib palash <golamhabibpalash@hotmail.com>
2021-10-15 10:56:19 +0600

    Customer Model Created

---
+++ MODELS/CommonProps.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MODELS
+{
+    public class CommonProps
+    {
+        public int Id { get; set; }
+
+        [Display(Name="Created By")]
+        public string CreatedBy { get; set; }
+
+        [Display(Name = "Created At")]
+        public DateTime CreatedAt { get; set; }
+
+        [Display(Name = "Updated By")]
+        public string UpdatedBy { get; set; }
+
+        [Display(Name = "Updated At")]
+        public DateTime UpdatedAt { get; set; }
+    }
+}
---
+++ MODELS/Customer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MODELS
+{
+    public class Customer : CommonProps
+    {
+        public string CustomerName { get; set; }
+        public string PhoneNo { get; set; }
+        public string Email { get; set; }
+    }
+}
